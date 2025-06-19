0bdd5bc304fd3a05ffbfc18b0efce00ea6d675b8
pratikkumartulo <pratiktulo42@gmail.com>
2025-06-19 10:45:43 +0530

    For loop added in HTML using jinja

---
+++ Session 6/employee.py
@@ -0,0 +1,27 @@
+employee_data = {
+    1:{
+        'name':'Pratik',
+        'age':20,
+        'position': "Director"
+    },
+    2:{
+        'name':'John',
+        'age':30,
+        'position': "Manager"
+    },
+    3:{
+        'name':'Alice',
+        'age':25,
+        'position': "Developer"
+    },
+    4:{
+        'name':'Bob',
+        'age':28,
+        'position': "Designer"
+    },
+    5:{
+        'name':'Charlie',
+        'age':35,
+        'position': "Analyst"
+    },
+}
